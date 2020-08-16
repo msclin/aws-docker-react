d5cd82024781ad3b2b964f69e8983049e29d38c8
clinedim <marc.clinedinst@gmail.com>
2020-08-16 16:47:50 -0400

    Fixed failing test.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was changed on the `feature` branch.</p>
+        <p>This change was made on the `feature` branch.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
